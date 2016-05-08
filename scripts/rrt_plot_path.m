83bc7cbfd667c42da10e6efa99f09515b4550fb5
Brandon Araki <br.araki@gmail.com>
2016-05-08 17:04:20 -0400

    in the middle of updating rrtplotter

--- scripts/rrt_plot_path.m
+++ scripts/rrt_plot_path.m
@@ -1,4 +1,7 @@
 data = load('sandbox/rrtpath.txt');
+obstacles = load('sandbox/obstacles.txt')
+hold on
 plot(data(:,1),data(:,2),'o-');
-hold on, axis equal, grid on;
+plot(obstacles(:,1),data(:))
+axis equal, grid on;
 pause
