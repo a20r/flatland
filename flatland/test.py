b51006ad2cb12cc5a1df8bc1ca474949fe283140
Alexander Wallar <alex@wallar.me>
2016-05-08 16:41:43 -0400

    Moved stuff:

--- flatland/test.py
+++ flatland/test.py
@@ -1,59 +1,16 @@
 #!/usr/bin/env python
 
-######################################################################
-# Software License Agreement (BSD License)
-#
-#  Copyright (c) 2010, Rice University
-#  All rights reserved.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions
-#  are met:
-#
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above
-#     copyright notice, this list of conditions and the following
-#     disclaimer in the documentation and/or other materials provided
-#     with the distribution.
-#   * Neither the name of the Rice University nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#  POSSIBILITY OF SUCH DAMAGE.
-######################################################################
 
-# Author: Mark Moll
+from ompl import base as ob
+from ompl import geometric as og
 
-try:
-    from ompl import base as ob
-    from ompl import geometric as og
-except:
-    # if the ompl module is not in the PYTHONPATH assume it is installed in a
-    # subdirectory of the parent directory called "py-bindings."
-    from os.path import abspath, dirname, join
-    import sys
-    sys.path.insert(0, join(dirname(dirname(abspath(__file__))),'py-bindings'))
-    from ompl import util as ou
-    from ompl import base as ob
-    from ompl import geometric as og
 
 def isStateValid(state):
     # Some arbitrary condition on the state (note that thanks to
     # dynamic type checking we can just call getX() and do not need
     # to convert state to an SE2State.)
     return state[0] < 8
+
 
 def planWithSimpleSetup():
     # create an SE2 state space
@@ -93,7 +50,7 @@ def planWithSimpleSetup():
         # print the simplified path
         print(ss.getSolutionPath().printAsMatrix())
         filename = 'path.txt'
-        target = open(filename,'w')
+        target = open(filename, 'w')
         target.write(ss.getSolutionPath().printAsMatrix())
         target.close()
 
@@ -113,11 +70,11 @@ def planTheHardWay():
     # create a random start state
     start = ob.State(space)
     start.random()
-    start()[0] = -5;
+    start()[0] = -5
     # create a random goal state
     goal = ob.State(space)
     goal.random()
-    goal()[0] = 5;
+    goal()[0] = 5
     # create a problem instance
     prmpdef = ob.ProblemDefinition(si)
     # set the start and goal states
@@ -143,24 +100,20 @@ def planTheHardWay():
     RRTsolved = RRTplanner.solve(1.0)
 
     if PRMsolved:
-        # get the goal representation from the problem definition (not the same as the goal state)
-        # and inquire about the found path
         path = prmpdef.getSolutionPath()
         print("Found solution:\n%s" % path)
-        filename = 'prmpath.txt'
-        target = open(filename,'w')
+        filename = 'sandbox/prmpath.txt'
+        target = open(filename, 'w')
         target.write(prmpdef.getSolutionPath().printAsMatrix())
         target.close()
     else:
         print("No solution found")
 
     if RRTsolved:
-        # get the goal representation from the problem definition (not the same as the goal state)
-        # and inquire about the found path
         path = rrtpdef.getSolutionPath()
         print("Found solution:\n%s" % path)
-        filename = 'rrtpath.txt'
-        target = open(filename,'w')
+        filename = 'sandbox/rrtpath.txt'
+        target = open(filename, 'w')
         target.write(rrtpdef.getSolutionPath().printAsMatrix())
         target.close()
     else:
