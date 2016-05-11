e7d54f8c99f62848e5a1fd67b0ba93db8b8dd66f
Alexander Wallar <alex@wallar.me>
2016-05-11 17:59:16 -0400

    Add plot to paper

--- scripts/plot_metrics.py
+++ scripts/plot_metrics.py
@@ -1,7 +1,6 @@
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
 import warnings
 import pandas
 
@@ -10,23 +9,28 @@ if __name__ == "__main__":
     warnings.simplefilter("ignore")
     sns.set_context("poster", font_scale=1)
     data = pandas.read_csv("data/data.csv")
+    data.replace("FeatureAgglomeration", "F. Agglomeration", inplace=True)
     hue = "n_obs"
     rrt = data[data["planner"] == "RRTConnect"]
     prm = data[data["planner"] == "PRM"]
     plt.subplot(3, 2, 1)
     ax = sns.barplot(x="transformer", y="path_length", hue=hue, data=rrt)
     ax.set(xlabel="")
+    ax.legend().remove()
     plt.title("RRTConnect")
     plt.subplot(3, 2, 2)
     ax = sns.barplot(x="transformer", y="path_length", hue=hue, data=prm)
     ax.set(xlabel="")
+    ax.legend().remove()
     plt.title("PRM")
     plt.subplot(3, 2, 3)
     ax = sns.barplot(x="transformer", y="duration", hue=hue, data=rrt)
     ax.set(xlabel="")
+    ax.legend().remove()
     plt.subplot(3, 2, 4)
     ax = sns.barplot(x="transformer", y="duration", hue=hue, data=prm)
     ax.set(xlabel="")
+    ax.legend().remove()
     plt.subplot(3, 2, 5)
     sns.barplot(x="transformer", y="num_failed", hue=hue, data=rrt)
     plt.subplot(3, 2, 6)
